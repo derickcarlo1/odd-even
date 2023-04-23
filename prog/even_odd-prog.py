9069218a685e81a5887dfe9f746024c0057e9c5d
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-23 18:37:15 +0800

    plotting pseudocodes

---
+++ prog/even_odd-prog.py
@@ -0,0 +1,8 @@
+# Open the numbers.txt file (reading)
+# Create two empty lists for even & odd numbers
+# Loop through each line in the file
+# Convert the line to an integer
+# Check if the number is even or odd and add it to the proper list
+# Close the file
+# Open the even.txt file for writing and write even numbers to it
+# Open the odd.txt file for writing and write odd numbers to it
