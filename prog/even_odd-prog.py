8aec7cfd0f6c5468f3e0687ada0a23a85e629005
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-23 18:45:19 +0800

    Create two empty lists for even and odd numbers (for loop, convert, check) and Close the file

--- prog/even_odd-prog.py
+++ prog/even_odd-prog.py
@@ -5,10 +5,19 @@ file = open("numbers.txt", "r")
 even_numbers = []
 odd_numbers = []
 
-
 # Loop through each line in the file
-# Convert the line to an integer
-# Check if the number is even or odd and add it to the proper list
+for line in file:
+    # Convert the line to an integer
+    number = int(line.strip())
+
+    # Check if the number is even or odd and add it to the appropriate list
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
+
 # Close the file
+file.close()
+
 # Open the even.txt file for writing and write even numbers to it
 # Open the odd.txt file for writing and write odd numbers to it
